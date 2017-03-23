3d756bbe160e6bc2ca30efd8bbb8597af9c241e0
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-03-23 13:53:22 -0700

    Update

---
+++ jsexercises.js
@@ -0,0 +1,34 @@
+//Bonus Points if you use comments!!!!
+
+
+/* In-N-Out
+Using prompts, make a game where you can order a burger or shake and the computer responds as the cashier.
+
+Bonus: 
+	Tally up the cost
+*/
+
+/* Fizz Buzz
+Write a function named fizzbuzz that goes from 1-100 and
+prints out 'fizz' if a number is divisible by 3
+prints out 'buzz ' if a number is divisible by 5
+prints out 'fizzbuzz' if a number is divisible by 15
+prints out the number if none of the other statements are true
+
+Bonus: Use an argument named 'limit' to make the function print from 1-limit
+*/
+
+
+
+/* Prime Numbers
+   Write a function that prints all of the prime numbers between 1-100
+*/
+
+
+/* Number Guessing Game 
+Make a game using prompts where the computer picks a number and you can guess it
+Bonus: 
+- Make there only 3 tries
+- Tell the user if their guess is too low or too high
+/*
+
